ad4ff94360eb4895ca63d597c2342af0fb61e63b
Agatha Wong <illuminating.aw@gmail.com>
2018-08-01 19:56:56 +0000

    Updated the connection class.

--- app/db/main/DatabaseConnection.py
+++ app/db/main/DatabaseConnection.py
@@ -11,8 +11,16 @@ class DatabaseConnection:
 
     def configureConnection(self):
         try:
-            dbConnection = mysql.connector.connect(self.username, self.password, self.host, self.database)
-        except Exception as e:
+            dbConnection = mysql.connector.connect(user = self.username, password = self.password, host = self.host, database = self.database)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+        else:
             dbConnection.close()
 
-conn = new DatabaseConnection("eggie", "eggie", "127.0.0.1", "Market")
+conn = DatabaseConnection('eggie', 'eggie', '127.0.0.1', 'Market')
+conn.configureConnection()
